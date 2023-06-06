e52ee150413efa56b7980a5c815773c531d7e173
Wilmer Arambula <terabytesoftw@gmail.com>
2023-06-06 17:39:20 -0400

    Validate csrf token.

--- src/js/sweetalert.js
+++ src/js/sweetalert.js
@@ -1,4 +1,3 @@
-// Function to handle the click on the "Block" or "Unblock" button
 function sweetAlertClickHandler(event) {
     event.preventDefault();
 
@@ -10,49 +9,46 @@ function sweetAlertClickHandler(event) {
     const url = event.currentTarget.href;
     const title = event.currentTarget.getAttribute('data-title') || 'Confirmation';
 
-    Swal.fire(
-        {
-            cancelButtonText: cancelText,
-            confirmButtonText: confirmText,
-            icon: icon,
-            reverseButtons: true,
-            showCancelButton: true,
-            text: message,
-            title: title
-        }
-    ).then(
-        (result) => {
-            if (result.isConfirmed) {
-                const form = document.createElement('form');
-                form.method = method;
-                form.action = url;
+    Swal.fire({
+        cancelButtonText: cancelText,
+        confirmButtonText: confirmText,
+        icon: icon,
+        reverseButtons: true,
+        showCancelButton: true,
+        text: message,
+        title: title
+    }).then((result) => {
+        if (result.isConfirmed) {
+            const form = document.createElement('form');
+            form.method = method;
+            form.action = url;
 
-                if (method.toUpperCase() === 'POST') {
-                    // Add CSRF token if using POST method
-                    const csrfToken = document.querySelector('meta[name="csrf"]').getAttribute('content');
+            if (method.toUpperCase() === 'POST') {
+                // Check if CSRF token is available
+                const csrfTokenElement = document.querySelector('meta[name="csrf"]');
+
+                if (csrfTokenElement) {
+                    const csrfToken = csrfTokenElement.getAttribute('content');
                     const csrfInput = document.createElement('input');
                     csrfInput.type = 'hidden';
                     csrfInput.name = '_csrf';
                     csrfInput.value = csrfToken;
                     form.appendChild(csrfInput);
                 }
-
-                document.body.appendChild(form);
-                form.submit();
             }
+
+            document.body.appendChild(form);
+            form.submit();
         }
-    );
+    });
 }
 
-// Event listener for elements with the data-action attribute
 const elementsWithDataAction = document.querySelectorAll('[data-action="sweetAlertClickHandler"]');
 
-elementsWithDataAction.forEach(
-    (element) => {
-        const action = element.getAttribute('data-action');
+elementsWithDataAction.forEach((element) => {
+    const action = element.getAttribute('data-action');
 
-        if (typeof window[action] === 'function') {
-            element.addEventListener('click', window[action]);
-        }
+    if (typeof window[action] === 'function') {
+        element.addEventListener('click', window[action]);
     }
-);
+});
